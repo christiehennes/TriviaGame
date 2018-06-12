c9813716e2c1a30fd5b2dcd2abdf4e79ab7d6da2
Christie Hennes <christiehennes@gmail.com>
2018-06-11 22:54:41 -0700

    Initalized questions as objects and display question to the screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let unansweredQuestions = 0;
-let currentQuestion = 0;
+let currentQuestionIndex = 0;
 
 let triviaQuestion = {
 
@@ -18,13 +18,45 @@ let questionsArray = [];
 
 //Initalize Trivia Questions
 
-function initalizeQuestion() {
+function initalizeQuestions() {
 
-    for (let i = 0; i < 5; i++){
-        let trivQ = triviaQuestion;
-        trivQ.question = "Question " + i + ": What is the answer?";
-        trivQ.answersArray = ["Answer 1", "Answer 2", "Answer 3", "Answer 4"];
-        trivQ.answerIndex = 2; 
-        questionsArray.push(trivQ);
-    }
+    new Question("Question 1: What is the answer?", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1);
+    new Question("Question 2: What is the answer?", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1);
+    new Question("Question 3: What is the answer?", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1);
+    new Question("Question 4: What is the answer?", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1);
+
 }
+
+//Question constructor 
+function Question(question, answer1, answer2, answer3, answer4, index){
+    
+    //Inialize object with values passed into the function
+    this.question = question;
+    this.answersArray = [answer1, answer2, answer3, answer4];
+    this.answerIndex = index; 
+   
+    //Add new object to the array
+    questionsArray.push(this);
+
+}
+
+function displayQuestion(){
+
+    console.log("Inside displayQuestion");
+
+    //Grab the current div for the question
+    let questionDiv = $('#question');
+
+    //Create a new div for the the question and append it to the question div
+    let questionDisplay = questionDiv.append('<div>');
+
+    //Display the text for the question
+    questionDisplay.text(questionsArray[currentQuestionIndex].question);
+    console.log(questionsArray);
+
+}
+
+
+//Begin the game 
+initalizeQuestions();
+displayQuestion();
